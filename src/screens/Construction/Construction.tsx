5d9c603b108247e5c58b0b488e36e32af74a700b
Shivang Rathod <rathod.shivang@gmail.com>
2023-02-26 17:38:04 +0530

    change

--- src/screens/Construction/Construction.tsx
+++ src/screens/Construction/Construction.tsx
@@ -8,7 +8,7 @@ export const Construction: NextPage = () => {
     <div className={styles.container}>
       <div className={styles.content}>
         <h3>About</h3>
-        <p>Page is coming soon! Probably</p>
+        <p>Shivang Rathod is a Digital Marketing Team Leader at leading Ecommerce Development Company, also an independent SEO-focused content writer and a digital marketing consultant in his free time. With his content writing and marketing strategies, Shivang helps businesses to achieve 10x growth in traffic and 2x growth in conversions YoY. He has years of combined experience in creating results-driven content and data-driven content marketing and SEO strategies. He used consults and work with different generous businesses to help or streamline their campaigns and writings.</p>
       </div>
     </div>
   )
