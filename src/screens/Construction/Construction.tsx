6667165c6ef057839b180e22016d48be21b0d355
Shivang Rathod <rathod.shivang@gmail.com>
2023-07-24 00:27:39 +0530

    c6

--- src/screens/Construction/Construction.tsx
+++ src/screens/Construction/Construction.tsx
@@ -6,14 +6,6 @@ import styles from './Construction.module.scss'
 export const Construction: NextPage = () => {
   return (
     <div className={styles.container}>
-      <div className={styles.image}>
-        <Image
-          src="https://source.unsplash.com/x-ghf9LjrVg"
-          alt="construction"
-          fill
-          priority
-        />
-      </div>
       <div className={styles.content}>
         <h3>Under Construction</h3>
         <p>Page is coming soon! Probably</p>
